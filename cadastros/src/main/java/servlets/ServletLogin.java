bb8e05a3bafc3c928073864355c979212f2e78c3
janilson <janilson@DESKTOP-8LOROQT>
2022-01-09 21:03:04 -0300

    atualização

---
+++ cadastros/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,62 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public ServletLogin() {
+        super();
+       
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+				
+		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			ModelLogin modelLogin = new ModelLogin();			
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			if(modelLogin.getLogin().equalsIgnoreCase("admin") 
+					&& modelLogin.getSenha().equalsIgnoreCase("admin")) {
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				
+				redirecionar.forward(request, response);
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe Login e Senha !");
+				redirecionar.forward(request, response);
+			}
+			
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe Login e Senha !");
+			redirecionar.forward(request, response);
+		}
+
+	}
+
+}
---
+++ cadastros/src/main/webapp/index.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>cadastro</title>
+</head>
+<body>
+<h1>Bem vindo !</h1>
+
+<form action="ServletLogin" method="post">
+
+<table>
+<tr>
+<td><label>Login</label></td>
+<td><input name="login" type="text"></td>
+</tr>
+<tr>
+<td><label>Senha</label></td>
+<td><input name="senha" type="password"></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="enviar"></td>
+</tr>
+</table>
+
+</form>
+<h4>${msg}</h4>
+</body>
+</html>
